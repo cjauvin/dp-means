cd13295e213f94ad1697c667a194175dc174e8cf
Christian Jauvin <cjauvin@gmail.com>
2012-07-13 23:57:56 -0400

    multidim

--- dp_means.py
+++ dp_means.py
@@ -1,9 +1,10 @@
 # Christian Jauvin
 # http://christianjauv.in
 # 2012-07-13
-# Python translation of this R code: https://github.com/johnmyleswhite/bayesian_nonparametrics/tree/master/code/dp-means
+# R to Python translation of: https://github.com/johnmyleswhite/bayesian_nonparametrics/tree/master/code/dp-means
 # Found on: http://www.johnmyleswhite.com/notebook/2012/06/26/bayesian-nonparametrics-in-r/
 # Based on: http://arxiv.org/pdf/1111.0352.pdf
+# (Adapted for multidimensional data)
 
 from numpy import *
 
@@ -12,8 +13,9 @@ def dp_means(data, Lambda, max_iters=100, tolerance=10e-3):
     n = len(data)
     k = 1
     assignments = ones(n)
-    mu_x = array((mean(data[:,0]),)) 
-    mu_y = array((mean(data[:,1]),))
+    mu = []
+    for d in range(data.ndim):
+        mu.append(array((mean(data[:, d]),)))
     is_converged = False
     n_iters = 0
     ss_old = float('inf')
@@ -23,24 +25,24 @@ def dp_means(data, Lambda, max_iters=100, tolerance=10e-3):
         for i in range(n):
             distances = repeat(None, k)
             for j in range(k):
-                distances[j] = (data[i, 0] - mu_x[j]) ** 2 + (data[i, 1] - mu_y[j]) ** 2
+                distances[j] = sum((data[i, d] - mu[d][j]) ** 2  for d in range(data.ndim))
             if min(distances) > Lambda:
                 k += 1
                 assignments[i] = k-1
-                mu_x = append(mu_x, data[i, 0])
-                mu_y = append(mu_y, data[i, 1])
+                for d in range(data.ndim):                    
+                    mu[d] = append(mu[d], data[i, d])
             else:
                 assignments[i] = argmin(distances)
         for j in range(k):
             if len(where(assignments == j)) > 0:
-                mu_x[j] = mean(data[assignments == j, 0])
-                mu_y[j] = mean(data[assignments == j, 1])
+                for d in range(data.ndim):
+                    mu[d][j] = mean(data[assignments == j, d])
         ss_new = 0      
         for i in range(n):
-            ss_new <- ss_new + (data[i, 0] - mu_x[assignments[i]]) ** 2 + (data[i, 1] - mu_y[assignments[i]]) ** 2    
+            ss_new <- ss_new + sum((data[i, d] - mu[d][assignments[i]]) ** 2 for d in range(data.ndim))
         ss_change = ss_old - ss_new
         is_converged = ss_change < tolerance  
-    return {'centers': array((mu_x, mu_y)).transpose(),
+    return {'centers': column_stack([mu[d] for d in range(data.ndim)]),
             'assignments': assignments,
             'k': k, 'n_iters': n_iters}
                         
@@ -51,13 +53,15 @@ if __name__ == '__main__':
     def generate_data(n=100):
         mu_x = array((0, 5, 10, 15))
         mu_y = array((0, 5, 0, -5))
-        data = empty(shape=(n, 3))
+        data = empty(shape=(n, 2))
+        classes = []
         for i in range(n):
             c = rnd.choice(range(4))
-            data[i] = (random.normal(mu_x[c], 1), random.normal(mu_y[c], 1), c)
-        return data
+            data[i] = (random.normal(mu_x[c], 1), random.normal(mu_y[c], 1))
+            classes.append(c)
+        return data, classes
 
-    data = generate_data(100)    
+    data, claases = generate_data()
     results = dp_means(data, 50)
     print 'found %d clusters' % len(set(results['assignments']))
     plt.scatter(data[:,0], data[:,1], c=results['assignments'])
